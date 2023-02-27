fa52616b034e7f7e1c8b586dba707682bc5c6e6c
Chimezie Enyinnaya <meziemichael@gmail.com>
2023-02-27 09:35:12 +0100

    displaying all posts

---
+++ src/views/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex items-center justify-between">
+    <h1 class="text-3xl font-bold">Posts</h1>
+
+    <RouterLink to="/create" class="btn btn-primary">New post</RouterLink>
+  </div>
+
+  <div class="mt-12 space-y-10">
+    <template v-if="posts.length > 0">
+      <div v-for="post in posts" :key="post.id">
+        <h2 class="text-2xl font-bold">{{ post.title }}</h2>
+
+        <div class="mt-2 flex items-center justify-between">
+          <p class="text-sm">{{ post.createdAt }}</p>
+
+          <div class="space-x-2">
+            <RouterLink to="#" class="text-sm btn btn-link btn-xs"> Edit </RouterLink>
+            <button class="text-sm btn btn-error btn-xs">Delete</button>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <div>No posts yet!</div>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { RouterLink } from 'vue-router'
+import axiosClient from '@/services/axios'
+import type Post from '@/types/post'
+
+const posts = ref<Post[]>([])
+
+onMounted(async () => {
+  const { data } = await axiosClient.get('/posts')
+
+  posts.value = data.data
+})
+</script>
