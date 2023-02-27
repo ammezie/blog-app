ad2dc831d5ec6897e0bcb54385924ae206b40ba0
Chimezie Enyinnaya <meziemichael@gmail.com>
2023-02-27 09:37:11 +0100

    creating  posts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PostList from '../views/PostList.vue'
+import CreatePost from '../views/CreatePost.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'posts',
       component: PostList
+    },
+    {
+      path: '/create',
+      name: 'createPost',
+      component: CreatePost
     }
   ]
 })
---
+++ src/views/CreatePost.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1 class="mb-8 text-3xl font-bold">Create post</h1>
+
+  <form class="space-y-4" @submit.prevent="createPost" method="post">
+    <div class="form-control">
+      <label class="label">Title</label>
+      <input
+        type="text"
+        class="input input-bordered"
+        placeholder="Post title"
+        v-model="post.title"
+      />
+    </div>
+    <div class="form-control">
+      <label class="label">Content</label>
+      <textarea
+        class="textarea textarea-bordered"
+        rows="10"
+        placeholder="Post content"
+        v-model="post.content"
+      ></textarea>
+    </div>
+
+    <button class="btn btn-primary">Create Post</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axiosClient from '@/services/axios'
+
+const router = useRouter()
+
+const post = ref({
+  title: '',
+  content: ''
+})
+
+async function createPost() {
+  await axiosClient.post('/posts', post.value)
+
+  router.push({ name: 'home' })
+}
+</script>
